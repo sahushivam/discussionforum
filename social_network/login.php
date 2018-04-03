ee60c1312bc74baf3af513f11024667e6b9c9feb
sahushivam <sahushivam25@gmail.com>
2018-04-03 18:48:02 +0530

    Update login.php

--- social_network/login.php
+++ social_network/login.php
@@ -5,11 +5,9 @@
 		$email=mysqli_real_escape_string($con,$_POST['uemail']);
 		$pass=mysqli_real_escape_string($con,$_POST['upass']);
 		echo $email.$pass;
-
 		$get_user="select * from users where user_email='$email' AND user_pass='$pass'";
 		$run_user=mysqli_query($con,$get_user);
 		$check=mysqli_num_rows($run_user);
-
 		if($check==1){
 			$_SESSION['user_email']=$email;
 			header('Location: home.php');
@@ -18,6 +16,5 @@
 		{
 			echo"<script>alert('email is not correct')</script>";
 		}
-
 	}
 ?>
